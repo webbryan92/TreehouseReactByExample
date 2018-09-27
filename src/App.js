6085280e137e72324b00bb3a5d186e29322afa9e
Ryan Webb <webbryan92@gmail.com>
2018-09-27 05:43:15 -0400

    refactoring the project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@
 import React, { Component } from 'react';
 import './App.css';
 //components
-import GuestList from './GuestList';
-import Counter from './Counter'
+import Header from './Header';
+import MainContent from './MainContent';
 
 class App extends Component {
   state = {
@@ -71,7 +71,7 @@ class App extends Component {
   }
 
   toggleFilter = () =>
-  this.setState({isFiltered: !this.state.isFiltered });
+    this.setState({ isFiltered: !this.state.isFiltered });
 
   handleNameInput = e =>
     this.setState({ pendingGuest: e.target.value });
@@ -94,7 +94,7 @@ class App extends Component {
   getTotalInvited = () => this.state.guests.length;
   getAttendingGuests = () =>
     this.state.guests.reduce(
-      (total, guest) => guest.isConfirmed ? total + 1: total, 0);
+      (total, guest) => guest.isConfirmed ? total + 1 : total, 0);
   //getUnconfirmedGuests = () =>
 
   render() {
@@ -103,29 +103,32 @@ class App extends Component {
     const numberUnconfirmed = totalInvited - numberAttending;
     return (
       <div className="App">
-        <header>
-          <h1>RSVP</h1>
-          <p>A Treehouse App</p>
-          <form onSubmit={this.newGuestSubmitHandler}>
-            <input 
-            type="text" 
-            onChange={this.handleNameInput}
-            value={this.state.pendingGuest} 
-            placeholder="Invite Someone" />
-            <button type="submit" name="submit" value="submit">Submit</button>
-          </form>
-        </header>
-        <div className="main">
-          <div>
-            <h2>Invitees</h2>
-            <label>
-              <input 
-                type="checkbox"
-                onChange={this.toggleFilter}
-                checked={this.state.isFiltered} /> Hide those who haven't responded
-            </label>
-          </div>
-          <Counter 
+        <Header
+          newGuestSubmitHandler={this.newGuestSubmitHandler}
+          pendingGuest={this.state.pendingGuest}
+          handleNameInput={this.handleNameInput}
+        />
+        <MainContent
+          toggleFilter={this.toggleFilter}
+          isFiltered={this.state.isFiltered}
+          totalInvited={totalInvited}
+          numberAttending={numberAttending}
+          numberUnconfirmed={numberUnconfirmed}
+          guests={this.state.guests}
+          toggleConfirmationAt={this.toggleConfirmationAt}
+          toggleEditingAt={this.toggleEditingAt}
+          setNameAt={this.setNameAt}
+          removeGuestAt={this.removeGuestAt}
+          pendingGuest={this.state.pendingGuest}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*<Counter 
             totalInvited = {totalInvited}
             numberAttending = {numberAttending}
             numberUnconfirmed = {numberUnconfirmed} />
@@ -137,12 +140,4 @@ class App extends Component {
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
             removeGuestAt={this.removeGuestAt}
-            pendingGuest={this.state.pendingGuest} />
-
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
+            pendingGuest={this.state.pendingGuest} />*/
--- src/MainContent/GuestList/index.js
+++ src/MainContent/GuestList/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Guest from './Guest.js'
-import PendingGuest from './PendingGuest';
+import PendingGuest from './PendingGuest.js';
 
 const GuestList = props =>
     <ul>
